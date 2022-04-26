9900c4aefe2d5462bab689d385fd3ef12d8adc97
Anna Spitz <aspitzemail@gmail.com>
2022-04-26 16:45:30 -0500

    Add script

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const clock = () => {let today = new Date();
+let day = today.getDay();
+let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday', 'Saturday'];
+let hour = today.getHours();
+let minute = today.getMinutes();
+let second = today.getSeconds();
+let period = 'AM';
+if (hour == 0) {
+  hour = 12;
+} else if (hour >= 12) {
+  hour = hour -  12;
+  period = 'PM';
+}
+hour = (hour < 10)? '0' + hour : hour;
+minute = (minute < 10)? '0' + minute : minute;
+second = (second < 10)? '0' + second : second;
 
+let time = `${hour} : ${minute} : ${second} ${period}`;
+document.getElementById('clock').innerText = time;
+setTimeout(clock, 1000);
+}
+clock();
